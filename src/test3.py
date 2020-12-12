7e83ff319a4d5c82cb72b463ab240b7dc9a00951
sunny-coffee <62189915+sunny-coffee@users.noreply.github.com>
2020-12-12 19:19:34 +0100

    12.12

---
+++ src/test3.py
@@ -0,0 +1,6 @@
+from allennlp.predictors.predictor import Predictor
+predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/bert-base-srl-2020.11.19.tar.gz")
+result=predictor.predict(
+  sentence="Did Uriah honestly think he could beat the game in under three hours?"
+)
+print(result)
